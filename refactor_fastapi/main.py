e345f1009059b2bbc49843236f420c112759348e
Anders Latif <andl@kea.dk>
2023-02-09 15:05:22 +0100

    Added database

--- refactor_fastapi/main.py
+++ refactor_fastapi/main.py
@@ -15,3 +15,12 @@ app.include_router(routers.timelines_router)
 app.include_router(routers.users_router)
 app.include_router(routers.auth_router)
 
+
+
+# todo delete this.. i have it as an example
+import database.connection as db
+
+cursor = db.cursor()
+cursor.execute('SELECT * FROM user LIMIT 4')
+
+
