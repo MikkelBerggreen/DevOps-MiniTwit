19b2e6df1e10c113a2deba084cea8ea60ba93ad2
Nedas Surkus <nedassurkus@gmail.com>
2023-02-25 21:56:46 +0100

    Update main_test.py
    Quick test

--- src/backend/main_test.py
+++ src/backend/main_test.py
@@ -76,7 +76,7 @@ class MiniTwitTestCase(unittest.TestCase):
         response = client.post(
             "/api/auth/login/",
             data={"username": "Test", "password": "Test"},
-            follow_redirects = True
+          allow_redirects = True
         )
         assert response.status_code == 201
         assert b'sign out [Test]' in response.read()
