9b72ce27ff91d86e36e325d0bfee85a7ff8aa5c9
Nedas Surkus <nedassurkus@gmail.com>
2023-03-01 12:48:57 +0000

    Modified tests. The build should fail.

--- src/backend/tests/new_tests/endpoint_tests/sim_api_test.py
+++ src/backend/tests/new_tests/endpoint_tests/sim_api_test.py
@@ -3,205 +3,177 @@ import base64
 import requests
 import unittest
 from fastapi.testclient import TestClient
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 from main import app
 
-BASE_URL = "http://127.0.0.1:5001"
-DATABASE = "/tmp/minitwit.db"
-USERNAME = "simulator"
-PWD = "super_safe!"
-CREDENTIALS = ":".join([USERNAME, PWD]).encode("ascii")
-ENCODED_CREDENTIALS = base64.b64encode(CREDENTIALS).decode()
-HEADERS = {
-    "Connection": "close",
-    "Content-Type": "application/json",
-    f"Authorization": f"Basic {ENCODED_CREDENTIALS}",
-}
+# In order for the tests to work. We need to override existing db connection url with a fake database
+# Afterwards we might take several approaches :
+# Revert transactions to prevent committing. Using fixture
+# Allow transactions but delete the database after all tests are done.
+# Have single instances of testing. Meaning we test a single function in a vacuum.
+# In this file. I Attempted to make the override but without a more concrete ORM implementation, 
+# it is hard to make tests / predict how the tests could look like. 
+
+# test_database.py
+SQLALCHEMY_DATABASE_URL = "postgresql://test-fastapi:password@db/test-fastapi-test"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Following line creates database
+# Base.metadata.create_all(bind=engine)
+
+
+def override_get_db():
+    try:
+        db = TestingSessionLocal()
+        db.begin()
+        yield db
+    finally:
+        db.rollback()
+        db.close()
+
+
+# Override db dependency.
+# app.dependency_overrides[get_db] = override_get_db
 
 
 client = TestClient(app)
 
 
+# Booking token test
+@pytest.mark.parametrize("user, email, pwd, latest",
+                         [("a", "a@a.a", "a", 1),
+                          ("b", "b@b.b", "b", 5),
+                          ("c", "c@c.c", "c", 6)])
+def test_register_NormalRegister(user, email, pwd, latest):
+    response = client.post(
+        "/register",
+        data={"username": user, "email": email, "pwd": pwd},
+        params={"latest": latest},
+    )
+    assert response.status_code == 204
+    # assert response.json() == {"success": "register success"} This would test register normally. Due to 204 it fails.
+
+    response = client.get("/latest")
+    assert response.status_code == 200
+    assert response.json() == {"latest": latest}
+
+
 class Simulation_API_Testing(unittest.TestCase):
-    @unittest.skip("No way to test it just yet")
-    def test_latest():
-        # post something to updaet LATEST
-        url = f"{BASE_URL}/register"
-        data = {"username": "test", "email": "test@test", "pwd": "foo"}
-        params = {"latest": 1337}
-        response = requests.post(
-            url, data=json.dumps(data), params=params, headers=HEADERS
+
+    def test_latest(self):
+        response = client.post(
+            "/register",
+            data={"username": "test", "email": "test@test", "pwd": "foo"},
+            params={"latest": 1337},
         )
-        assert response.ok
+        assert response.status_code == 204
+
+        response = client.get("/latest")
+        assert response.status_code == 200
+        assert response.json() == {"latest": 1337}
+
+    def test_create_msg_for_user_a(self):
+        response = client.post(
+            "/msgs/a",
+            data={"content": "Blub"},
+            params={"latest": 2},
+        )
+        assert response.status_code == 204
 
         # verify that latest was updated
-        url = f"{BASE_URL}/latest"
-        response = requests.get(url, headers=HEADERS)
-        assert response.ok
-        assert response.json()["latest"] == 1337
+        response = client.get("/latest")
+        assert response.status_code == 200
+        assert response.json() == {"latest": 2}
 
-    @unittest.skip("No way to test it just yet")
-    def test_register():
-        username = "a"
-        email = "a@a.a"
-        pwd = "a"
-        data = {"username": username, "email": email, "pwd": pwd}
-        params = {"latest": 1}
-        response = requests.post(
-            f"{BASE_URL}/register",
-            data=json.dumps(data),
-            headers=HEADERS,
-            params=params,
+    def test_get_latest_user_msgs(self):
+
+        response = client.get(
+            "/msgs/a",
+            params={"no": 20, "latest": 3},
         )
-        assert response.ok
-        # TODO: add another assertion that it is really there
+        assert response.status_code == 204
 
+        # impossible to test due to 204 status code.
+        # got_it_earlier = False
+        # for msg in response.json():
+        #   if msg["content"] == "Blub!" and msg["user"] == username:
+        #        got_it_earlier = True
+
+        # assert got_it_earlier
         # verify that latest was updated
-        response = requests.get(f"{BASE_URL}/latest", headers=HEADERS)
-        assert response.json()["latest"] == 1
+        response = client.get("/latest")
+        assert response.status_code == 200
+        assert response.json() == {"latest": 3}
 
-    @unittest.skip("No way to test it just yet")
-    def test_create_msg():
-        username = "a"
-        data = {"content": "Blub!"}
-        url = f"{BASE_URL}/msgs/{username}"
-        params = {"latest": 2}
-        response = requests.post(
-            url, data=json.dumps(data), headers=HEADERS, params=params
+    def test_get_latest_msgs(self):
+        response = client.get(
+            "/msgs",
+            params={"no": 20, "latest": 4},
         )
-        assert response.ok
-
-        # verify that latest was updated
-        response = requests.get(f"{BASE_URL}/latest", headers=HEADERS)
-        assert response.json()["latest"] == 2
-
-    @unittest.skip("No way to test it just yet")
-    def test_get_latest_user_msgs():
-        username = "a"
-
-        query = {"no": 20, "latest": 3}
-        url = f"{BASE_URL}/msgs/{username}"
-        response = requests.get(url, headers=HEADERS, params=query)
         assert response.status_code == 200
 
         got_it_earlier = False
         for msg in response.json():
-            if msg["content"] == "Blub!" and msg["user"] == username:
+            if msg["content"] == "Blub!" and msg["user"] == "a":
                 got_it_earlier = True
 
         assert got_it_earlier
 
-        # verify that latest was updated
-        response = requests.get(f"{BASE_URL}/latest", headers=HEADERS)
-        assert response.json()["latest"] == 3
-
-    @unittest.skip("No way to test it just yet")
-    def test_get_latest_msgs():
-        username = "a"
-        query = {"no": 20, "latest": 4}
-        url = f"{BASE_URL}/msgs"
-        response = requests.get(url, headers=HEADERS, params=query)
+        response = client.get("/latest")
         assert response.status_code == 200
+        assert response.json() == {"latest": 4}
 
-        got_it_earlier = False
-        for msg in response.json():
-            if msg["content"] == "Blub!" and msg["user"] == username:
-                got_it_earlier = True
+    def test_follow_user(self):
 
-        assert got_it_earlier
-
-        # verify that latest was updated
-        response = requests.get(f"{BASE_URL}/latest", headers=HEADERS)
-        assert response.json()["latest"] == 4
-
-    @unittest.skip("No way to test it just yet")
-    def test_register_b():
-        username = "b"
-        email = "b@b.b"
-        pwd = "b"
-        data = {"username": username, "email": email, "pwd": pwd}
-        params = {"latest": 5}
-        response = requests.post(
-            f"{BASE_URL}/register",
-            data=json.dumps(data),
-            headers=HEADERS,
-            params=params,
+        response = client.post(
+            "/fllws/a",
+            data={"follow": "b"},
+            params={"latest": 7},
         )
-        assert response.ok
-        # TODO: add another assertion that it is really there
+        assert response.status_code == 204
 
-        # verify that latest was updated
-        response = requests.get(f"{BASE_URL}/latest", headers=HEADERS)
-        assert response.json()["latest"] == 5
-
-    @unittest.skip("No way to test it just yet")
-    def test_register_c():
-        username = "c"
-        email = "c@c.c"
-        pwd = "c"
-        data = {"username": username, "email": email, "pwd": pwd}
-        params = {"latest": 6}
-        response = requests.post(
-            f"{BASE_URL}/register",
-            data=json.dumps(data),
-            headers=HEADERS,
-            params=params,
+        response = client.post(
+            "/fllws/a",
+            data={"follow": "c"},
+            params={"latest": 8},
         )
-        assert response.ok
+        assert response.status_code == 204
 
-        # verify that latest was updated
-        response = requests.get(f"{BASE_URL}/latest", headers=HEADERS)
-        assert response.json()["latest"] == 6
-
-    @unittest.skip("No way to test it just yet")
-    def test_follow_user():
-        username = "a"
-        url = f"{BASE_URL}/fllws/{username}"
-        data = {"follow": "b"}
-        params = {"latest": 7}
-        response = requests.post(
-            url, data=json.dumps(data), headers=HEADERS, params=params
+        response = client.get(
+            "/fllws/a",
+            params={"no": 20, "latest": 9},
         )
-        assert response.ok
-
-        data = {"follow": "c"}
-        params = {"latest": 8}
-        response = requests.post(
-            url, data=json.dumps(data), headers=HEADERS, params=params
-        )
-        assert response.ok
-
-        query = {"no": 20, "latest": 9}
-        response = requests.get(url, headers=HEADERS, params=query)
-        assert response.ok
-
+        assert response.status_code == 200
         json_data = response.json()
         assert "b" in json_data["follows"]
         assert "c" in json_data["follows"]
 
         # verify that latest was updated
-        response = requests.get(f"{BASE_URL}/latest", headers=HEADERS)
-        assert response.json()["latest"] == 9
+        response = client.get("/latest")
+        assert response.status_code == 200
+        assert response.json() == {"latest": 9}
 
-    @unittest.skip("No way to test it just yet")
-    def test_a_unfollows_b():
-        username = "a"
-        url = f"{BASE_URL}/fllws/{username}"
-
-        #  first send unfollow command
-        data = {"unfollow": "b"}
-        params = {"latest": 10}
-        response = requests.post(
-            url, data=json.dumps(data), headers=HEADERS, params=params
+    def test_a_unfollows_b(self):
+        response = client.post(
+            "/fllws/a",
+            data={"unfollow": "b"},
+            params={"latest": 10},
         )
-        assert response.ok
+        assert response.status_code == 204
 
         # then verify that b is no longer in follows list
-        query = {"no": 20, "latest": 11}
-        response = requests.get(url, params=query, headers=HEADERS)
-        assert response.ok
+        response = client.get(
+            "/fllws/a",
+            params={"no": 20, "latest": 11},
+        )
+        assert response.status_code == 200
         assert "b" not in response.json()["follows"]
 
-        # verify that latest was updated
-        response = requests.get(f"{BASE_URL}/latest", headers=HEADERS)
-        assert response.json()["latest"] == 11
+        response = client.get("/latest")
+        assert response.status_code == 200
+        assert response.json() == {"latest": 11}
