beceaa5193eaa5840a2dab3b71504c6272bff7d0
Nedas Surkus <nedassurkus@gmail.com>
2023-03-12 16:26:03 +0100

    removed unused tests

--- src/backend/tests/new_tests/endpoint_tests/orm_queries_test.py
+++
@@ -1,7 +0,0 @@
-from services.implementions.user_service import User_Service
-
-user_service = User_Service()
-
-#def test_user():
-#    followers = user_service.get_all_followers(1, 5)
-#    assert len(followers) > 0
